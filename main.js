2419911bfb357dd88c67c27db0114bff565ffe4a
Shuvo Mallik <shuvomallik999@gmail.com>
2025-08-26 23:46:21 +0600

    aaaa

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 /*script for login succesfully*/
-const number = '123456789';
-const pin = '2580';
-document.getElementById('btn-login')
-.addEventListener('click', function(event){
-const userNumber = document.getElementById('input-number')
-    .value;
- const userPin = document.getElementById('input-pin')
- .value;
- if(userNumber === number && userPin === pin){
-    window.open("homepage.html" , "block")
- }
- else{
-    console.log('Wrong');
- }
-})
+const number = "123456789";
+const pin = "2580";
+document
+  .getElementById("btn-login")
+  .addEventListener("click", function (event) {
+    const userNumber = document.getElementById("input-number").value;
+    const userPin = document.getElementById("input-pin").value;
+    if (userNumber === number && userPin === pin) {
+      window.open("addMoney.html", "block");
+    } else {
+      console.log("Wrong");
+    }
+  });
