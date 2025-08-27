5878f13c8403f9a787248226daffcfbbe00d0887
Shuvo Mallik <shuvomallik999@gmail.com>
2025-08-27 17:08:04 +0600

    toggle done

--- home.js
+++ home.js
@@ -1,21 +1,53 @@
+
+function cheakCard(id){
+    const btnCheak = document.getElementById(id);
+    btnCheak.style.backgroundColor = 'rgba(8, 116, 242, 0.05)';
+    btnCheak.style.border = "1px solid #0874F2";
+}
+
+function toggleCard(){
+    const btnCards = document.getElementsByClassName('card');
+        for(const card of btnCards){
+            card.style.backgroundColor = 'white';
+            card.style.border = "rgba(8,8,8,0.2)";
+        }
+}
+
+function toggleBtn(currentId){
+const forms = document.getElementsByClassName('forms');
+ for(const form of forms){
+    console.log(form);
+    form.style.display = "none";
+ }
+const currentForm = document.getElementById(currentId);
+ currentForm.style.display = 'block'
+}
+
+
 document.getElementById('add-money')
 .addEventListener('click', function(){
-    const addMoney = document.getElementById('add-money-form');
-    const latestPayment = document.getElementById('latest-payment');
-    addMoney.style.display = 'block';
-    latestPayment.style.display = 'none';
-    const addMoneyCard = document.getElementById('add-money');
-    addMoneyCard.style.backgroundColor = 'rgba(8, 116, 242, 0.05';
-    addMoneyCard.style.border = "1px solid #0874F2";
+    toggleBtn('add-money-form');
+        toggleCard();
+        cheakCard('add-money')
 })
-
 document.getElementById('cash-out-money')
 .addEventListener('click', function(){
-    const cashOutMoney = document.getElementById('cash-out-form');
-    const latestPayment = document.getElementById('latest-payment');
-    cashOutMoney.style.display = 'block';
-    latestPayment.style.display = 'none';
-    const cashOutMoneyCard = document.getElementById('cash-out-money');
-    cashOutMoneyCard.style.backgroundColor = 'rgba(8, 116, 242, 0.05';
-    cashOutMoneyCard.style.border = "1px solid #0874F2";  
+    toggleBtn('cash-out-form');
+     toggleCard();
+     cheakCard('cash-out-money')
+
+})
+
+document.getElementById('transfer-money')
+.addEventListener('click', function(){
+    toggleBtn('transfer-form');
+     toggleCard();
+     cheakCard('transfer-money')
+})
+
+document.getElementById('get-bonus')
+.addEventListener('click', function(){
+toggleBtn('get-bonus-form');
+toggleCard();
+cheakCard('get-bonus')
 })
